51e5962299491307bfbdfbdf196e91fe2fefd020
bailichuan <508065909@qq.com>
2025-04-13 15:16:58 +0800

    hot-fix 99乘法表

--- test.go
+++ test.go
@@ -6,4 +6,12 @@ import (
 
 func main() {
 	fmt.Println("hello world")
+	//99乘法表
+	for i := 1; i <= 9; i++ {
+		for j := 1; j <= i; j++ {
+			fmt.Printf("%d*%d=%d ", i, j, j*i)
+		}
+		fmt.Println()
+	}
+
 }
