2cb776b6a7737c768819f86528257de1b2883ffb
bailichuan <508065909@qq.com>
2025-04-13 15:22:07 +0800

    master 数组排序遍历

--- test.go
+++ test.go
@@ -6,4 +6,18 @@ import (
 
 func main() {
 	fmt.Println("hello world")
+	//冒泡排序
+	var arr [10]int = [...]int{43, 32, 5, 7, 8, 98, 65, 432, 32, 2}
+	for i := 0; i < len(arr)-1; i++ {
+		for j := 0; j < len(arr)-i-1; j++ {
+			if arr[j] > arr[j+1] {
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+			}
+		}
+	}
+	//遍历
+	for i, v := range arr {
+		fmt.Printf("arr[%d]=%d\n", i, v)
+	}
+
 }
